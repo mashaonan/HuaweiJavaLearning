c41620ccf03324b288595cc8a65e6dc2e40886b4
msn <745154135@qq.com>
2022-05-28 16:49:01 +0800

    modified calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator {
     public int compute(int name1, int name2, char symbol) {
-        return -1;
+        switch (symbol) {
+            case '+':
+                return name1 + name2;
+            case '-':
+                return name1 - name2;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
